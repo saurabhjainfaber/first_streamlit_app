c70c7e66b11cd5598f6c09d36a2ec9dc4833e4c1
saurabhjainfaber <123saurabhjain@gmail.com>
2023-08-01 21:54:46 +0530

    try if else added

--- streamlit_app.py
+++ streamlit_app.py
@@ -23,16 +23,17 @@ streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
 
+try:
 fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
+if not fruit_choice:
+  streamlit.error("Please select a fruit to get information")
+else:  
+  fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
+  fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
+  streamlit.dataframe(fruityvice_normalized)
 
-
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+ fruit_choice)
-
-# map key and value of json 
-fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
-# add json in a frame for display
-streamlit.dataframe(fruityvice_normalized)
+except URLError as e:
+  streamlit.error()
 
 streamlit.stop()
 
